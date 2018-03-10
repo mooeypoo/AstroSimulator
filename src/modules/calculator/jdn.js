61c2398c2e29e1914267ff2d55f7189472c2fcf6
Moriel Schottlender <moriel@gmail.com>
2018-03-10 00:39:17 -0800

    Replacing JDN method mathematal algorithm

--- src/modules/calculator/jdn.js
+++ src/modules/calculator/jdn.js
@@ -5,9 +5,8 @@
  * JDN is used for astronomical calculations.
  * See https://en.wikipedia.org/wiki/Julian_day
  *
- * The algorithm below was adjusted from the algorithm
- * in the page:
- * https://quasar.as.utexas.edu/BillInfo/JulianDateCalc.html
+ * The algorithm below was adapted from
+ * http://aa.quae.nl/en/reken/juliaansedag.html
  *
  * @param {number} [year] Requested year (YYYY)
  * @param {number} [month] Requested month (MM)
@@ -17,34 +16,23 @@
  * @return {number} The decimal number of days from January 1, 4713 BC, 0:00 UT
 */
 const getJDN = function ( year, month, day, hour = 12, minute = 0 ) {
-	let A, B, C, E, F, julianDay, julianDayTime;
+	const c0 = Math.floor( ( month - 3 ) / 12 ),
+		x4 = year + c0,
+		x3 = Math.floor( x4 / 100 ), // Quotient
+		x2 = x4 % 100, // Remainder
+		x1 = month - 12 * c0 - 3,
+		jdn = Math.floor( 146097 * x3 / 4 ) +
+			Math.floor( 36525 * x2 / 100 ) +
+			Math.floor( ( 153 * x1 + 2 ) / 5 ) +
+			day + 1721119;
 
-	if ( month < 3 ) {
-		year--;
-		month += 12;
-	}
-	// Gregorian calendar corrections
-	A = Math.floor( year / 100 );
-	B = Math.floor( A / 4 );
-	C = 2 - A + B;
-	E = Math.floor( 365.25 * ( year + 4716 ) );
-	F = Math.floor( 30.6001 * ( month + 1 ) );
-	julianDay = C + day + E + F - 1524;
-	julianDayTime = ( hour - 12 ) / 24 + 	minute / 1440;
+	// Add time of day consideration
+	let jdnTime = ( hour - 12 ) / 24 + 	minute / 1440;
 
-	return julianDay + julianDayTime;
-	// let jdn = (
-	// 	// Days
-	// 	( ( 1461 * ( year + 4800 + ( month - 14 ) / 12 ) ) / 4 ) +
-	// 	( ( 367 * ( month - 2 - 12 * ( ( month - 14 ) / 12 ) ) ) / 12 ) -
-	// 	( ( 3 * ( ( year + 4900 + ( month - 14 ) / 12 ) / 100 ) ) / 4 ) +
-	// 	day - 32075 // +
-	// 	// // Hours and minutes
-	// 	// ( ( hour - 12 ) / 24 ) +
-	// 	// ( minute / 1440 )
-	// );
-// return jdn;
-	// return Math.round( jdn * 100 ) / 100;
+	// Round time to at most 3 decimal places
+	jdnTime = Math.round( jdnTime * 1000 + Number.EPSILON ) / 1000;
+
+	return jdn + jdnTime;
 };
 
 module.exports = {
--- test/test.calculator.jdn.js
+++ test/test.calculator.jdn.js
@@ -35,6 +35,26 @@ describe( 'Julian Day operations', function () {
 				date: [ 2000, 1, 1, 0, 0 ],
 				expected: 2451544.5,
 				msg: 'Known JDN for January 1st, 2000 00:00 UTC'
+			},
+			{
+				date: [ 2011, 7, 29, 14, 0 ],
+				expected: 2455772.083,
+				msg: 'Known JDN for July 7th, 2011 14:00 UTC'
+			},
+			{
+				date: [ 2010, 9, 7 ],
+				expected: 2455447,
+				msg: 'Known JDN for September 7th, 2010 12:00 UTC'
+			},
+			{
+				date: [ 2100, 2, 28 ],
+				expected: 2488128,
+				msg: 'Known JDN for February 28th, 2100 12:00 UTC'
+			},
+			{
+				date: [ 2000, 2, 29 ],
+				expected: 2451604,
+				msg: 'Known JDN for February 29th, 2000 12:00 UTC'
 			}
 		];
 
